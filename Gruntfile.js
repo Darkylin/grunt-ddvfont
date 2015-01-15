a99fcf14df72fa8616674bcf7fc7a3fb2f29a261
darkylin <darkylin1989@gmail.com>
2015-01-15 15:33:04 +0800

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,6 @@ module.exports = function(grunt) {
       }
     }
   });
-
-  grunt.ddvfont = require('./ddv-gruntify')(grunt);
+  grunt.loadTasks('tasks');
 
 };
---
+++ tasks/ddv-gruntify.js
@@ -0,0 +1,23 @@
+/*
+ * ddvfont
+ *
+ * Copyright (c) 2015 Darkylin
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.registerMultiTask('ddvfont', 'ddv for crawler', function() {
+        //console.log(this);
+        var ddv = require('../lib/DDVcreator'),done=this.async();
+        switch(this.target){
+            case 'create':
+                ddv.create(this.options()).done(done);
+                break;
+            default:
+                grunt.log.errorlns('任务目标目前仅支持 create');
+        }
+    });
+
+};
